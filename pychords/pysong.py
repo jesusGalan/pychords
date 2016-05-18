a9503fc87870d1d30f3aadd6711cb8badce7c9b9
jesusGalan <jgc.developments@outlook.com>
2016-05-18 02:20:52 +0200

    Added new tools over main features

---
+++ pychords/pysong.py
@@ -0,0 +1,92 @@
+#  -*- coding: utf-8 -*-
+
+import os
+import sys
+import wave
+
+import pysynth
+import pyaudio
+
+
+
+if __name__ == '__main__':
+
+    pychord_module = __import__('functions')
+    scale_grade = sys.argv[1]
+    note = sys.argv[2]
+    escala = pychord_module.scales(scale_grade, note)
+    octave = 4
+
+    n = 4
+    c = 2
+    sc = 1
+    b = 8
+    r = 16
+
+    def prepare_for_pysynth(_list):
+        _new_list = []
+        for elem in _list:
+            _new_list.append(tuple([elem.lower() + str(octave), n]))
+
+        _new_list.append(tuple([_list[0].lower() + str(int(octave) + 1), n]))
+
+        return tuple(_new_list)
+
+    test = prepare_for_pysynth(escala)
+
+    pysynth.make_wav(test, fn="test.wav")
+
+    # define stream chunk
+    chunk = 1024
+
+    # open a wav format music
+    _root = ''
+    _name = ''
+    path = ''
+    for root, dirs, files in os.walk('.'):
+        for name in files:
+            if 'test.wav' in name:
+                _root = root
+                _name = name
+                path = os.path.realpath(os.path.join(root, name))
+
+    f = wave.open(path, "rb")
+    # instantiate PyAudio
+    p = pyaudio.PyAudio()
+    # open stream
+    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                    channels=f.getnchannels(),
+                    rate=f.getframerate(),
+                    output=True)
+    # read data
+    data = f.readframes(chunk)
+
+    # paly stream
+    while data != '':
+        stream.write(data)
+        data = f.readframes(chunk)
+
+    # stop stream
+    stream.stop_stream()
+    stream.close()
+
+    # close PyAudio
+    p.terminate()
+    f.close()
+    x = raw_input('want to keep this wav file? y/n: ')
+
+    if x == 'y':
+        try:
+            os.rename(os.path.realpath(os.path.join(_root, _name)), os.path.realpath(os.path.join(_root, scale_grade + '_in_' + note + '.wav')))
+
+        except WindowsError:
+            print '\n'
+            print 'The file is already in the current directory. No changes applied.'
+            print '\n'
+
+    elif x == 'n':
+        os.remove(os.path.realpath(os.path.join(_root, _name)))
+
+    else:
+        print 'mmmakey'
+        os.remove(os.path.realpath(os.path.join(_root, _name)))
--- readme.txt
+++ readme.txt
@@ -15,6 +15,24 @@ escala turca septimo grado
 escala turca tercer grado
 escala lidia b7
 
+You can create wav files of the scale via pychords/pysong.py SCALE_NAME NOTE
+
+NOTE: You need to do pip install pyaudio while virtual enviroment(recommended) is running
+
+Here an example:
+
+>python pychords/pysong.py escala_mayor_natural C
+
+Writing to file test.wav
+[1/8]
+[5/8]
+
+want to keep this wav file? y/n: y
+
+
+The file is already in the current directory. No changes applied.
+
+
 Copyright (C) 2016  jgalanc
 
     This program is free software: you can redistribute it and/or modify
@@ -40,3 +58,13 @@ tested with expects and mamba:
 	- Mamba: pip install mamba
 
     >> mamba
+
+Stream scales on fly with pyaudio:
+
+	- pip install pyaudio
+
+Sounds synth provided with pysynth:
+
+	- pip install PySynth-1.1.tar.gz
+
+	NOTE: This file is the main folder
